bc4a9c5660c13ae103bfe70fe9719e4bff7cb759
louis mane <dennis.itua1@gmail.com>
2022-10-29 14:23:05 -0400

    Updated node_modules

--- publisher.js
+++ publisher.js
@@ -8,7 +8,7 @@ async function connect () {
         const connection = await amqp.connect("amqp://localhost:5672");
         const channel = await connection.createConfirmChannel();
         await channel.assertQueue("jobs");
-        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(message)))
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(message)))
         console.log(`job sent successfully ${message.num}`);
         await channel.close();
         await connection.close();
